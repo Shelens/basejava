7d6a3d29c8cd1c3818b073db014a08972a1e374e
Shelens <laniva83@gmail.com>
2023-01-13 21:02:02 +0100

    улучшены методы в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,23 +6,25 @@ import java.util.Arrays;
 
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    int storageSize = size();
 
     void clear() {
-        for (int i = 0; i <  storage.length; i++){
+        for (int i = 0; i < storageSize; i++) {
             storage[i] = null;
         }
+        storageSize = 0;
     }
 
     void save(Resume r) {
-            storage[size()] = r;
+        storage[storageSize] = r;
+        storageSize = storageSize + 1;
 
     }
 
-    Resume get(String uuid)  {
+    Resume get(String uuid) {
         Resume resume = null;
-        for (Resume res : storage){
-            if (res != null && res.uuid.equals(uuid)){
+        for (Resume res : storage) {
+            if (res != null && res.uuid.equals(uuid)) {
                 resume = res;
             }
         }
@@ -30,30 +32,32 @@ public class ArrayStorage {
     }
 
     void delete(String myUuid) {
-        for (int i = 0; i < storage.length; i++){
-            if (storage[i] != null && storage[i].uuid.equals(myUuid)){
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(myUuid)) {
                 int index = i;
             }
         }
-        for (int i = 0; i < storage.length-1; i++){
-            storage[i] = storage[i+1];
+        for (int i = 0; i < storageSize - 1; i++) {
+            storage[i] = storage[i + 1];
         }
-        storage[storage.length-1] = null;
-        }
+        storage[storageSize - 1] = null;
+        storageSize--;
+    }
 
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allResumes = Arrays.copyOf(storage, size());
+        Resume[] allResumes = Arrays.copyOf(storage, storageSize);
         return allResumes;
     }
 
     int size() {
         int count = 0;
-        for(Resume res : storage){
-            if (res != null) ++count;}
+        for (Resume res : storage) {
+            if (res != null) ++count;
+        }
         return count;
     }
 }
