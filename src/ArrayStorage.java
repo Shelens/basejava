1f98074a8b91b03b3b43f70a91311cb807d72e1c
Shelens <laniva83@gmail.com>
2023-01-14 12:49:12 +0100

    обновлены методы в классе ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -17,40 +17,43 @@ public class ArrayStorage {
 
     void save(Resume r) {
         storage[storageSize] = r;
-        storageSize = storageSize + 1;
+        storageSize += 1;
 
     }
 
     Resume get(String uuid) {
-        Resume resume = null;
-        for (Resume res : storage) {
-            if (res != null && res.uuid.equals(uuid)) {
-                resume = res;
-            }
-        }
-        return resume;
+        for (int i = 0; i < storageSize; i++) {
+            if (storage[i].uuid.equals(uuid)) return storage[i];
+       }
+        return  null;
     }
 
     void delete(String myUuid) {
+        int index = 0;
+        boolean resumeFound = false;
         for (int i = 0; i < storageSize; i++) {
-            if (storage[i] != null && storage[i].uuid.equals(myUuid)) {
-                int index = i;
+            if (storage[i].uuid.equals(myUuid)) {
+                 index = i;
+                 resumeFound = true;
             }
         }
-        for (int i = 0; i < storageSize - 1; i++) {
+        if (resumeFound == false){
+            System.out.println("resume not found");
+        } else {
+        for (int i = index; i < storageSize - 1; i++) {  //1 2 3
             storage[i] = storage[i + 1];
         }
         storage[storageSize - 1] = null;
         storageSize--;
-    }
+    }}
 
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] allResumes = Arrays.copyOf(storage, storageSize);
-        return allResumes;
+        return Arrays.copyOf(storage, storageSize);
+
     }
 
     int size() {
